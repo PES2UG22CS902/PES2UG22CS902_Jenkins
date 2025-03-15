040fc28afde0785abc40541597c57d597c6ee1aa
Adam <150950632+PES2UG22CS902@users.noreply.github.com>
2025-03-15 21:51:08 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I(PES2UG22CS902) have successfully built and run this  \n";
+  cout << "Webhooks!";
+  cout << "pipeline";
+  return 0;
+}
